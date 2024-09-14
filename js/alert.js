ad17834892ee0a171ac083928d9801bee638a3a7
dandilesmana <dandilesmana.if21@gmail.com>
2024-09-14 21:22:22 +0700

    update

--- js/alert.js
+++ js/alert.js
@@ -3,3 +3,6 @@ document.getElementById('bookingForm').onsubmit = function(event) {
     alert("Booking berhasil disimpan!"); // Menampilkan pesan sukses
     this.submit(); // Lanjutkan pengiriman form setelah alert
 };
+
+const top = document.getElementById('atas');
+top.style.color = '#2596be';
